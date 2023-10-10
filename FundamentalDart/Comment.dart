4baef13858374cc6419ce9a1f3424b363668feeb
mastam3003 <projectmagangjogjacom@gmail.com>
2023-10-10 15:31:47 +0700

    jenis-jenis comment

---
+++ FundamentalDart/Comment.dart
@@ -0,0 +1,10 @@
+void main(){
+  // Comments ( kode yang ditulis tapi tidak dijalankan ) terdiri dari 3 jenis :
+  // Single line comment
+  /* Multi line comment */
+  /// atau /**  docuemntation comment *// ///
+  print (" Jenis - jenis komentar yang digunakan oleh Dart : ");
+  print(" 1. Single line comment (//)");
+  print(" 2. Multi line comment (/*....*/");
+  print(" 3. Docuemntation comment (/** atau /// ......../// atau *///");
+}
