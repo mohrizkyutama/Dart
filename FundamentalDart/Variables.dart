26e9bda889f28b8db89e767a3d33b2428d53d6d8
mastam3003 <projectmagangjogjacom@gmail.com>
2023-10-10 15:47:28 +0700

    jenis-jenis comment

---
+++ FundamentalDart/Variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  // Keyword var untuk definsi sebuah variabel pada Dart
+  /* (1) = (assignment operator) berfungsi memasukan nilai ke variabel
+     (2) Proses memasukan nilai ke variabel dinamakan inisialisasi
+     (3) Deklarasi variabel adalah menyimpan nama dan ruang tanpa memberikan nilai
+   */
+  var name = "Tama";
+  var age; // Deklarasi Variabel
+  age = 20;
+  print(name);
+  print("Nilai diatas berasal dari variabel name");
+  print("Dibawah ini adalah contoh deklrasi variabel");
+  print(age);
+}
